0d3bcf4134350e8047bf25b9aed3209776d77d08
Aleksey Dobrovolsky <a.dobrovolsky1991@gmail.com>
2019-03-04 12:32:21 +0300

    feat: initial implementation of finding zeroes of factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+     // number=20;
+     let i;
+// base=10;
+    let maxFactors = [];
+    let exp;
+    let zeroCount = 0;
+    let factors = getFactors(base);
+    for (i = 0; i<factors.length; i++) {
+        if (factors[i].first >= factors[i].second && maxFactors.indexOf(factors[i].first) == -1) {
+            maxFactors.push(factors[i].first)
+        } else if (factors[i].first < factors[i].second && maxFactors.indexOf(factors[i].second) == -1) maxFactors.push(factors[i].second);
+    }
+    for (i = 0; i<maxFactors.length; i++){
+        exp = 1;
+        while (Math.pow(maxFactors[i],exp)<=number){
+            zeroCount += Math.floor(number/Math.pow(maxFactors[i],exp));
+            exp++;
+        }
+    }
+    return zeroCount;
+}
+
+function getFactors(base) {
+    let factors = [];
+    var simpleDigits = getSimpleDigits(base);
+    for (let i = 0; i < simpleDigits.length; i++) {
+        if (base % simpleDigits[i] == 0) factors.push({first: simpleDigits[i], second: base / simpleDigits[i]});
+    }
+    return factors;
+}
+
+function getSimpleDigits(base){
+    let simpleDigits = [];
+    let isSimple = true;
+    for (let i = 2; i <= base; i++) {
+        isSimple = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isSimple = false;
+                break;
+            }
+        }
+        if (isSimple)simpleDigits.push(i);
+    }
+    return simpleDigits;
 }
