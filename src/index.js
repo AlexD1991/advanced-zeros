edd67ae598a9a21665c94dfbcb65c7c5eba4a297
Aleksey Dobrovolsky <a.dobrovolsky1991@gmail.com>
2019-03-04 17:35:56 +0300

    fix: fix formula for checking zeroes

--- src/index.js
+++ src/index.js
@@ -1,47 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-     // number=20;
-     let i;
-// base=10;
-    let maxFactors = [];
+    let count=[];
     let exp;
-    let zeroCount = 0;
-    let factors = getFactors(base);
-    for (i = 0; i<factors.length; i++) {
-        if (factors[i].first >= factors[i].second && maxFactors.indexOf(factors[i].first) == -1) {
-            maxFactors.push(factors[i].first)
-        } else if (factors[i].first < factors[i].second && maxFactors.indexOf(factors[i].second) == -1) maxFactors.push(factors[i].second);
-    }
-    for (i = 0; i<maxFactors.length; i++){
+    let factorsAndExp = getFactorsAndExp(base);
+    for (let i=0; i<factorsAndExp.length;i++){
         exp = 1;
-        while (Math.pow(maxFactors[i],exp)<=number){
-            zeroCount += Math.floor(number/Math.pow(maxFactors[i],exp));
+        count[i]=0;
+        while (Math.pow(factorsAndExp[i].digit,exp)<=number){
+            count[i] += Math.floor(number/Math.pow(factorsAndExp[i].digit,exp));
             exp++;
         }
+        count[i]=Math.floor(count[i]/factorsAndExp[i].exp);
     }
-    return zeroCount;
+    return Math.min(...count);
 }
 
-function getFactors(base) {
-    let factors = [];
-    var simpleDigits = getSimpleDigits(base);
-    for (let i = 0; i < simpleDigits.length; i++) {
-        if (base % simpleDigits[i] == 0) factors.push({first: simpleDigits[i], second: base / simpleDigits[i]});
-    }
-    return factors;
-}
+function getFactorsAndExp(base){
+    let factorsAndExp = [];
+    let exp=1;
+    let baseQuotient = base;
 
-function getSimpleDigits(base){
-    let simpleDigits = [];
-    let isSimple = true;
-    for (let i = 2; i <= base; i++) {
-        isSimple = true;
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) {
-                isSimple = false;
-                break;
-            }
+    for (let i=2; i<=base; i++){
+        exp=0;
+        while (baseQuotient % i === 0){
+            exp++;
+            baseQuotient = baseQuotient / i;
         }
-        if (isSimple)simpleDigits.push(i);
+        if (exp!=0) factorsAndExp.push({digit: i, exp: exp} );
     }
-    return simpleDigits;
+    return factorsAndExp;
 }
